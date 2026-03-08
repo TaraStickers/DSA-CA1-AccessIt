8c2bcb1e722b8db2b633d6798eb4c35c165cd260
Tara <x24158216@student.ncirl.ie>
2026-03-08 18:24:13 +0000

    0.1 GUI Structure
    basic structure of GUI's

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dsa.ca1;
+
+/**
+ *
+ * @author taraj
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new MenuGUI().setVisible(true);
+        });
+    }
+}
