969d54fe9a0f54d8e42e64885d9efe7c5c467fd9
Tara <x24158216@student.ncirl.ie>
2026-03-10 10:15:57 +0000

    0.4 - subclasses for routes

---
+++ WalkingRoute.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dsa.ca1;
+
+/**
+ *
+ * @author Tara
+ */
+public class WalkingRoute extends Route {
+    private boolean hasStairs;
+    
+    public WalkingRoute (String routeID, String start, String end, double distance, boolean hasStairs){
+        super(routeID, start, end, distance); //parent
+        this.hasStairs = hasStairs;   
+    }
+    @Override
+    public String toString() {
+        //append extra details onto super toStirng method
+        return super.toString() + "\n Type of route: Walking route" + "\nHas stairs: " + hasStairs;
+    }
+}
---
+++ WheelchairRoute.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dsa.ca1;
+
+/**
+ *
+ * @author Tara
+ */
+public class WheelchairRoute extends Route{
+    private boolean hasLifts;
+    private boolean hasRamps;
+    
+    public WheelchairRoute(String routeID, String start, String end, double distance, boolean hasLifts, boolean hasRamps) {
+        super(routeID, start, end, distance);
+        this.hasRamps = hasRamps;
+        this.hasLifts = hasLifts;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + "\nType of route: Wheelchair route" + "\nHas ramps: " + hasRamps + "\nHas lifts: " + hasLifts;
+    }
+}
